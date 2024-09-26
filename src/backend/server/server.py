58449e9d4ad638a9f962269f9e6e065668e35366
3DA <3da@darijanavric.dev>
2024-09-26 16:41:25 +0200

    Server(feature): endpoints added

--- src/backend/server/server.py
+++ src/backend/server/server.py
@@ -3,8 +3,20 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def hello_root():
+    return 'Hello from root!'
+
+@app.route('/data')
+def hello_website():
+    return 'Hello from website!'
+
+@app.route('/email')
+def hello_email():
+    return 'Hello from email!'
+
+@app.route('/db')
+def hello_db():
+    return 'Hello from database!'
 
 if __name__ == '__main__':
     app.run(debug=True)
