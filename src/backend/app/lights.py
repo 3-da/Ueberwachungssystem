2e3e9bbfecd34f16e9e92a4cee0db952f948b9ef
darijan_avric <3da@darijanavric.dev>
2024-09-28 10:15:16 +0200

    Lights(feature): RYG added

---
+++ src/backend/app/lights.py
@@ -0,0 +1,67 @@
+import RPi.GPIO as GPIO
+import time
+
+
+class Lights:
+    def __init__(self, red,yellow,green):
+        self.red = int(red)
+        self.yellow = int(yellow)
+        self.green = int(green)
+
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.red, GPIO.OUT)
+        GPIO.setup(self.yellow, GPIO.OUT)
+        GPIO.setup(self.green, GPIO.OUT)
+
+        self.all_off()
+
+    def red_on(self):
+        GPIO.output(self.red, GPIO.HIGH)
+    
+    def red_off(self):
+        GPIO.output(self.red, GPIO.LOW)
+
+    def yellow_on(self):
+        GPIO.output(self.yellow, GPIO.HIGH)
+    
+    def yellow_off(self):
+        GPIO.output(self.yellow, GPIO.LOW)
+
+    def green_on(self):
+        GPIO.output(self.green, GPIO.HIGH)
+    
+    def green_off(self):
+        GPIO.output(self.green, GPIO.LOW)
+
+    def all_off(self):
+        self.red_off()
+        self.yellow_off()
+        self.green_off()
+
+    def cleanup(self):
+        self.all_off()
+        GPIO.cleanup()
+
+if __name__ == "__main__":
+    red = 26
+    yellow = 19
+    green = 13
+
+    lights = Lights(red, yellow, green)
+
+    try:
+        lights.red_on()
+        time.sleep(2)
+        lights.red_off()
+
+        lights.yellow_on()
+        time.sleep(2)
+        lights.yellow_off()
+
+        lights.green_on()
+        time.sleep(2)
+        lights.green_off()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        lights.cleanup()
