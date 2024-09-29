0276191611abbb6c0f22d27ca2d302b5661c0b15
Nils <nils.cherubin@gmail.com>
2024-09-29 15:37:58 +0200

    Füge Webpages hinzu

--- src/backend/server/server.py
+++ src/backend/server/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, request, redirect, url_for
 
 from src.backend.email.send_email import EmailSender
 
@@ -8,8 +8,20 @@ app = Flask(__name__)
 def hello_root():
     return 'Hello from root!'
 
-@app.route('/data')
-def serve_website():
+# Login-Route, die sowohl GET als auch POST unterstützt
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # Abfrage der Formulardaten
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        if username == 'admin' and password == 'p123':  # Beispiel für einfache Authentifizierung
+            return redirect(url_for('dashboard'))
+        else:
+            return redirect(url_for('failed'))
+
+    # Bei GET-Anfrage wird das Login-Formular angezeigt
     return send_from_directory('../../frontend', 'index.html')
 
 @app.route('/static/css/<path:filename>')
@@ -29,6 +41,16 @@ def hello_email():
 @app.route('/db')
 def hello_db():
     return 'Hello from database!'
+
+# Fehlerseite bei falschem Login
+@app.route('/failed', methods=['GET'])
+def failed():
+    return send_from_directory('../../frontend', 'failed_login.html')
+
+# Dashboard-Seite
+@app.route('/dashboard')
+def dashboard():
+    return send_from_directory(directory='../../frontend', path='dashboard.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
