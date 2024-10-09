e7008b099fb27046484b6ab161e02254cad32ae3
3DA <3da@darijanavric.dev>
2024-10-09 08:49:22 +0200

    Server(feature) - can't access dashboard without logging in

--- src/backend/server/server.py
+++ src/backend/server/server.py
@@ -1,13 +1,16 @@
-from flask import Flask, send_from_directory, render_template, request, redirect, url_for, jsonify
+from flask import Flask, send_from_directory, render_template, request, redirect, url_for, session, jsonify
 from src.backend.app.send_email import EmailSender
-from src.backend.database.database import Admin, Entrie, Breakin, Error, OnCallDuty, session
+# from src.backend.database.database import Admin, Entrie, Breakin, Error, OnCallDuty, session
 
 app = Flask(__name__, template_folder='../../frontend', static_folder='../../frontend/static')
+app.secret_key = '1234'
 
 auth_error = {
     "username": None,
     "password": None
 }
+
+logged_in = False
 
 @app.route('/')
 def index():
@@ -24,6 +27,7 @@ def login():
         auth_error['password'] = password != 'p123'
 
         if not auth_error['username'] and not auth_error['password']:
+            session['logged_in'] = True
             return redirect(url_for('dashboard'))
         return redirect(url_for('index', auth_error=auth_error))
 
@@ -48,35 +52,37 @@ def hello_email():
     email_sender.send_email("Einbruch!", "Bewegung erkannt!")
     return 'Email sent!'
 
-@app.route('/add_admin', methods=['POST'])
-def add_admin():
-    rfid = request.form.get('rfid')
-    password = request.form.get('password')
-    oncall = request.form.get('oncall') == 'on'
-    img = request.form.get('img')
-    name = request.form.get('name')
-    email = request.form.get('email')
-    phone = request.form.get('phone')
-
-    new_admin = Admin(
-        rfid=rfid,
-        password=password,
-        oncall=oncall,
-        img=img,
-        name=name,
-        email=email,
-        phone=phone
-    )
-    session.add(new_admin)
-    session.commit()
-    return jsonify({"message": "New admin added successfully!"}), 201
+# @app.route('/add_admin', methods=['POST'])
+# def add_admin():
+#     rfid = request.form.get('rfid')
+#     password = request.form.get('password')
+#     oncall = request.form.get('oncall') == 'on'
+#     img = request.form.get('img')
+#     name = request.form.get('name')
+#     email = request.form.get('email')
+#     phone = request.form.get('phone')
+#
+#     new_admin = Admin(
+#         rfid=rfid,
+#         password=password,
+#         oncall=oncall,
+#         img=img,
+#         name=name,
+#         email=email,
+#         phone=phone
+#     )
+#     session.add(new_admin)
+#     session.commit()
+#     return jsonify({"message": "New admin added successfully!"}), 201
 
 
 
 
 @app.route('/dashboard')
 def dashboard():
-    return send_from_directory(directory='../../frontend', path='dashboard.html')
+    if not session.get('logged_in'):
+        return redirect(url_for('index'))
+    return render_template('dashboard.html')
 
 
 if __name__ == '__main__':
