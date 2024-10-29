cad1a53f3e123df1116efebb480e1ad556fa8b27
darijan_avric <3da@darijanavric.dev>
2024-10-29 10:06:51 +0100

    Edit motion sensor

--- src/backend/app/motion_sensor.py
+++ src/backend/app/motion_sensor.py
@@ -1,28 +1,38 @@
 import RPi.GPIO as GPIO
 import time
+# import neopixel
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
 MOTION_SENSOR = 21
 ROT = 26
+RGB = 18
+
+# pixels = neopixel.NeoPixel(RGB, 24)
 
 GPIO.setup(ROT, GPIO.OUT)
 GPIO.setup(MOTION_SENSOR, GPIO.IN)
+GPIO.setup(RGB, GPIO.OUT)
+
+# def set_color(color):
+#    pixels.fill(color)
+#    pixels.show()
+
 
 def light():
     GPIO.output(ROT, True)
-    time.sleep(1)
+    time.sleep(0.3)
     GPIO.output(ROT, False)
-    time.sleep(1)
+    time.sleep(0.3)
 
 while True:
     motion = GPIO.input(MOTION_SENSOR)
     print(motion)
     if (motion):
-        # light()
-        GPIO.output(ROT, True)
-    else:
-        GPIO.output(ROT, False)
+        light()
+        # GPIO.output(RGB, True)
+        # set_color((255,0,0))
+    # else:
+        # GPIO.output(RGB, False)
     
-
