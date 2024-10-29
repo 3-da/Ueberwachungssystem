0e3385944da9811c075674abb0a6ddef19e9305e
darijan_avric <3da@darijanavric.dev>
2024-10-29 12:19:24 +0100

    Edit rfid sensor

--- src/backend/app/rfid_sensor.py
+++ src/backend/app/rfid_sensor.py
@@ -1,12 +1,18 @@
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
-
+# GPIO.cleanup()
 reader = SimpleMFRC522()
 
-# GPIO.setwarnings(False)
+GPIO.setwarnings(False)
 
-while True:
+try:
+    print("try")
     id, text = reader.read()
-    print(id)
-    print(text)
+    print(f"ID: {id}")
+    print(f"Text: {text.strip()}")
 
+except Exception as e:
+    print(f"Fehler: {e}")
+
+finally:
+    GPIO.cleanup()
