7bce836a6bf17811f418830f8626f758bc1d06b5
3DA <3da@darijanavric.dev>
2025-02-09 11:32:41 +0100

    Server(feature): run app

--- src/backend/app/app.py
+++ src/backend/app/app.py
@@ -49,12 +49,3 @@ class App:
         self.door_sensor.cleanup()
         self.motion_sensor.cleanup()
         self.lights.cleanup()
-
-
-if __name__ == "__main__":
-    app = App()
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        app.stop()
--- src/backend/server/server.py
+++ src/backend/server/server.py
@@ -1,11 +1,12 @@
 from flask import Flask, send_from_directory, render_template, request, redirect, url_for, session
 from src.backend.app.send_email import EmailSender
 from src.backend.database.add_admins import HandleAdmins
+from src.backend.app.app import App
 
 # from src.backend.database.database import session, Admin
 
-app = Flask(__name__, template_folder='../../frontend', static_folder='../../frontend/static')
-app.secret_key = '1234'
+server = Flask(__name__, template_folder='../../frontend', static_folder='../../frontend/static')
+server.secret_key = '1234'
 
 auth_error = {
     "username": None,
@@ -16,12 +17,12 @@ logged_in = False
 
 add_admins = HandleAdmins()
 
-@app.route('/')
+@server.route('/')
 def index():
     return render_template('index.html', auth_error=auth_error)
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@server.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form.get('username')
@@ -37,24 +38,24 @@ def login():
                 auth_error['password'] = 'Username or password is incorrect'
                 return redirect(url_for('index', auth_error=auth_error))
 
-@app.route('/logout')
+@server.route('/logout')
 def logout():
     session['logged_in'] = False
     return redirect(url_for('index'))
 
-@app.route('/dashboard')
+@server.route('/dashboard')
 def dashboard():
     if not session.get('logged_in'):
         return redirect(url_for('index'))
     return render_template('dashboard.html')
 
-@app.route('/add-admin', endpoint='add-admin')
+@server.route('/add-admin', endpoint='add-admin')
 def sign_up():
     if not session.get('logged_in'):
         return redirect(url_for('index'))
     return render_template('add_admin.html')
 
-@app.route('/admin-to-db', methods=['POST'])
+@server.route('/admin-to-db', methods=['POST'])
 def add_admin():
     if not session.get('logged_in'):
         return redirect(url_for('index'))
@@ -70,7 +71,7 @@ def add_admin():
 
         return redirect(url_for('dashboard'))
 
-@app.route('/remove-admin', methods=['GET', 'POST'])
+@server.route('/remove-admin', methods=['GET', 'POST'])
 def remove_admin():
     if not session.get('logged_in'):
         return redirect(url_for('index'))
@@ -82,52 +83,25 @@ def remove_admin():
 
         return redirect(url_for('dashboard'))
 
-@app.route('/show-admins', endpoint='show-admins')
+@server.route('/show-admins', endpoint='show-admins')
 def show_admins():
     if not session.get('logged_in'):
         return redirect(url_for('index'))
     admins = add_admins.get_admins()
     return render_template('show_admins.html', admins=admins)
 
-@app.route('/static/css/<path:filename>')
+@server.route('/static/css/<path:filename>')
 def serve_css(filename):
     return send_from_directory('../../frontend/static/css', filename)
 
 
-@app.route('/static/js/<path:filename>')
-def serve_js(filename):
-    return send_from_directory('../../frontend/static/js', filename)
-
-
-@app.route('/email')
+@server.route('/email')
 def hello_email():
     email_sender = EmailSender()
     email_sender.send_email("Einbruch!", "Bewegung erkannt!")
     return 'Email sent!'
 
-# @app.route('/add_admin', methods=['POST'])
-# def add_admin():
-#     rfid = request.form.get('rfid')
-#     password = request.form.get('password')
-#     oncall = request.form.get('oncall') == 'on'
-#     img = request.form.get('img')
-#     name = request.form.get('name')
-#     email = request.form.get('email')
-#     phone = request.form.get('phone')
-#
-#     new_admin = Admin(
-#         rfid=rfid,
-#         password=password,
-#         oncall=oncall,
-#         img=img,
-#         name=name,
-#         email=email,
-#         phone=phone
-#     )
-#     session.add(new_admin)
-#     session.commit()
-#     return jsonify({"message": "New admin added successfully!"}), 201
-
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app = App()
+    app.run()
+    server.run(debug=True)
