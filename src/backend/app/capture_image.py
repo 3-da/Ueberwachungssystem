9b64fca4983c63ef0ec74214eeccd5fae02bd208
daniel pauels <daniel-pauels@web.de>
2025-05-13 11:10:21 +0200

    Add timestamp for image caputre

--- src/backend/app/capture_image.py
+++ src/backend/app/capture_image.py
@@ -1,19 +1,26 @@
 import picamera
 import time
+from datetime import datetime
 
 class CaptureImage:
-    def __init__(self):
-        self.image_path = '/home/it/Ueberwachungssystem/src/backend/app/admin.jpg'
+    def __init__(self, image_path=None):
+        self.image_path = image_path 
 
     def capture(self):
+        # Set timestamp for image capture
+        timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        self.image_path = self.image_path or f"/home/it/Dokumente/Raumüberwachung II/Ueberwachungssystem/src/backend/app/breakin_{timestamp}.jpg"
+
         with picamera.PiCamera() as camera:
             camera.resolution = (640, 480)
             camera.start_preview()
-            time.sleep(5)
-
+            time.sleep(1)
+            
+            # Capture image
             camera.capture(self.image_path)
-            print(f"Image saved to {self.image_path}")
-
+            
+            print(f"Image saved to {self.image_path} with timestamp {timestamp}")
+            
             camera.stop_preview()
             camera.close()
 
