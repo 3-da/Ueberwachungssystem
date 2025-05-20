3b7f3c749486489513cf31b0e4b0420f897d51e6
NilsCtrl <nils.cherubin@gmail.com>
2025-05-20 14:36:09 +0200

    Add motion detection popup

--- src/backend/app/motion_sensor.py
+++ src/backend/app/motion_sensor.py
@@ -1,5 +1,7 @@
 import RPi.GPIO as GPIO
 import time
+import tkinter as tk
+import threading
 from capture_image import CaptureImage
 
 GPIO.setwarnings(False)
@@ -21,6 +23,33 @@ def light():
     GPIO.output(ROT, False)
     time.sleep(0.3)
 
+def show_popup():
+    popup = tk.Tk()
+    popup.title("Benachrichtigung")
+
+    # Festlegen der Fenstergröße
+    window_width = 300
+    window_height = 150
+
+    # Bildschirmgröße abrufen
+    screen_width = popup.winfo_screenwidth()
+    screen_height = popup.winfo_screenheight()
+
+    # Berechnung der x und y Koordinaten für die mittige Position
+    x_position = (screen_width // 2) - (window_width // 2)
+    y_position = (screen_height // 2) - (window_height // 2)
+
+    # Fenstergröße und Position setzen
+    popup.geometry(f"{window_width}x{window_height}+{x_position}+{y_position}")
+    
+    label = tk.Label(popup, text="Es wurde eine Bewegung im Raum erkannt!")
+    label.pack(pady=10)
+
+    # Fenster automatisch nach 3 Sekunden schließen
+    popup.after(3000, popup.destroy)
+
+    popup.mainloop()
+
 while True:
     motion = GPIO.input(MOTION_SENSOR)
     print(motion)
@@ -28,9 +57,11 @@ while True:
 
     current_time = time.time()
     if motion and (current_time - last_capture_time >= 6):  # Prüfen, ob 6 Sek vergangen sind
-        light()
         capture_image = CaptureImage()
         capture_image.capture()
         last_capture_time = current_time  # Aktualisieren der Zeit des letzten Captures
+        # light()
+        threading.Thread(target=show_popup, daemon=True).start()
+
         
     
